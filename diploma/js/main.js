8321c54eb41c70f147de5ef3c69894c156e5b236
Welasso <Vipwelasso@mail.ru>
2025-08-25 01:07:33 +0600

    Add files via upload

---
+++ diploma/js/main.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+ loop: true,
+
+ pagination: {
+  el: '.swiper-pagination',
+  clickable: true,
+ },
+
+ navigation: {
+  nextEl: '.section__new-arrow-right',
+  prevEl: '.section__new-arrow-left',
+ },
+
+ scrollbar: {
+  el: '.swiper-scrollbar',
+ },
+
+
+});
+
