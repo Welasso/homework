148c43ffbe6bffb7f93bf22eaf09a03a49bb2e18
Welasso <Vipwelasso@mail.ru>
2025-09-11 02:33:25 +0600

    Add files via upload

---
+++ diploma/js/main.js
@@ -0,0 +1,54 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+
+  },
+
+  navigation: {
+    nextEl: '.section__new-arrow-right',
+    prevEl: '.section__new-arrow-left',
+  },
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+
+  },
+});
+
+  // =========================================
+  
+(function () {
+  const selectors = [
+    '.electroHub__gadgets-listone',
+    '.electroHub__gadgets-listtwo',
+    '.electroHub__gadgets-listthree',
+    '.electroHub__gadgets-listfour'
+  ].join(', ');
+
+  const cards = document.querySelectorAll(selectors);
+
+  cards.forEach(card => {
+    card.addEventListener('click', () => {
+      // Закрываем все блоки кроме текущего
+      cards.forEach(c => {
+        if (c !== card) {
+          c.classList.remove('card--active');
+        }
+      });
+
+      // Переключаем текущий блок
+      card.classList.toggle('card--active');
+    });
+  });
+})();
+
+// =========================================≈
+
+
+
+
+// =========================================≈
